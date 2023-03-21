2ecc599f4ff26b8dc136fa69c8c01dc382edaf51
LimonJake <jakelimon.dev@gmail.com>
2023-03-21 16:54:56 -0600

    InMemoryUser

--- example-users-in-memo/src/main/java/com/jake/example/config/ProjectSecurityConfig.java
+++ example-users-in-memo/src/main/java/com/jake/example/config/ProjectSecurityConfig.java
@@ -3,6 +3,11 @@ package com.jake.example.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,6 +21,36 @@ public class ProjectSecurityConfig {
                 .and().formLogin()
                 .and().httpBasic();
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+
+//        UserDetails admin = User.withDefaultPasswordEncoder()
+//                .username("admin")
+//                .password("12345")
+//                .authorities("admin").build();
+//        UserDetails user = User.withDefaultPasswordEncoder()
+//                .username("user")
+//                .password("12345")
+//                .authorities("read").build();
+//
+//        return new InMemoryUserDetailsManager(admin, user);
+
+        UserDetails admin = User.withUsername("admin")
+                .password("12345")
+                .authorities("admin").build();
+        UserDetails user = User.withUsername("user")
+                .password("12345")
+                .authorities("read").build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 
 }
