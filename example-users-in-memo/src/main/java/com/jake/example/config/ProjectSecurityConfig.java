97beb691ba685316d1e5f8c48b3e9520c1bd5de6
LimonJake <jakelimon.dev@gmail.com>
2023-03-22 17:07:05 -0600

    Implementing JDBC auth

--- example-users-in-memo/src/main/java/com/jake/example/config/ProjectSecurityConfig.java
+++ example-users-in-memo/src/main/java/com/jake/example/config/ProjectSecurityConfig.java
@@ -2,15 +2,22 @@ package com.jake.example.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.sql.DataSource;
+
 @Configuration
+@EnableWebSecurity
 public class ProjectSecurityConfig {
 
     @Bean
@@ -23,19 +30,22 @@ public class ProjectSecurityConfig {
         return http.build();
     }
 
-    @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
 
-//        UserDetails admin = User.withDefaultPasswordEncoder()
-//                .username("admin")
-//                .password("12345")
-//                .authorities("admin").build();
-//        UserDetails user = User.withDefaultPasswordEncoder()
-//                .username("user")
-//                .password("12345")
-//                .authorities("read").build();
-//
-//        return new InMemoryUserDetailsManager(admin, user);
+    // The next example is using In memory authentication
+    /*
+    @Bean
+    public UserDetailsService userDetailsManager(){
+
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("12345")
+                .authorities("admin").build();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .authorities("read").build();
+
+        return new InMemoryUserDetailsManager(admin, user);
 
         UserDetails admin = User.withUsername("admin")
                 .password("12345")
@@ -46,11 +56,36 @@ public class ProjectSecurityConfig {
 
         return new InMemoryUserDetailsManager(admin, user);
     }
+*/
 
+
+    // No we will work with jdbc auth using postgresql
+//    @Bean
+//    public UserDetailsService userDetailsService(DataSource dataSource){
+//        return new JdbcUserDetailsManager(dataSource);
+//    }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+    public UserDetailsService users(DataSource dataSource) {
+        UserDetails user = User.builder()
+                .username("user")
+                .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW")
+                .roles("USER", "ADMIN")
+                .build();
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+        users.createUser(user);
+        users.createUser(admin);
+        return users;
     }
+
+//    @Bean
+//    public PasswordEncoder passwordEncoder(){
+//        return NoOpPasswordEncoder.getInstance();
+//    }
 
 }
